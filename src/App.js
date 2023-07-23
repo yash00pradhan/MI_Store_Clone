70085cd0b8bdf4cce2e16088fca863e0368cc99f
Yash Pradhan <yashpradhan0000@gmail.com>
2023-07-23 18:27:06 +0530

    Project
    ...

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import  Prenavbar from './component/Prenavbar';
+import  Navbar  from './component/Navbar';
+import {  BrowserRouter as Router } from "react-router-dom"
+import Slider from "./component/Slider.js"
+import data from "./data/data.json"
+import Offers from "./components/Offers.js"
+import Heading  from "./components/Heading.js"
+import StarProduct from "./components/StarProduct.js"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+     <Prenavbar/>
+     <Navbar/>
+     {/* <NavOptios miPhones={data.miPhones} redmiPhones={data.redmiPhones} tv={data.tv} laptop={data.laptop} fitnessAndLifeStyle={data.fitnessAndLifeStyle} home={data.home} audio={data.audio}  accessories={data.accessories}/> */}
+     
+     <Slider start={data.banner.start} />
+       <Offers offer={data.offer} />
+       <Heading text="STAR PRODUCTS"/>
+       <StarProduct starProduct={data.starProduct} />
+     </Router>
   );
 }
 
